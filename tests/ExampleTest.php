c59280e595ce0e1673a567f252366f6c87a39819
Marc Wolf <kontakt@marcit.eu>
2024-06-11 15:20:32 +0200

    [TASK] Add PHPUnit and test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
